146cd38dab82829bcb22d1651bd1df81d890f601
mito <miro.rucka@gmail.com>
2022-10-07 15:04:56 +0200

    init commit

---
+++ src/main/java/online/labmaster/taposmartplug/TapoSmartplugApplication.java
@@ -0,0 +1,13 @@
+package online.labmaster.taposmartplug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapoSmartplugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapoSmartplugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/labmaster/taposmartplug/TapoSmartplugApplicationTests.java
@@ -0,0 +1,13 @@
+package online.labmaster.taposmartplug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapoSmartplugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
