c4e8af00f673ef5add3af7dc1c561a290523be5f
mito <miro.rucka@gmail.com>
2022-10-11 20:06:30 +0200

    register another metrics to prometheus output.

--- src/main/java/online/labmaster/taposmartplug/api/inbound/DeviceInfoResponse.java
+++ src/main/java/online/labmaster/taposmartplug/api/inbound/DeviceInfoResponse.java
@@ -1,38 +1,91 @@
 package online.labmaster.taposmartplug.api.inbound;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class DeviceInfoResponse extends TapoResponse {
 
     public DeviceInfo result;
 
     public static class DeviceInfo {
-        public String device_id;
-        public String fw_ver;
-        public String hw_ver;
-        public String type;
-        public String model;
-        public String mac;
-        public String hw_id;
-        public String fw_id;
-        public String oem_id;
-        public String ip;
-        public int time_diff;
-        public String ssid;
-        public String rssi;
-        public String signal_level;
-        public String latitude;
-        public String longitude;
-        public String lang;
-        public String avatar;
-        public String region;
-        public String specs;
-        public String nickname;
-        public boolean has_set_location_info;
-        public boolean device_on;
-        public int on_time;
-        public boolean overheated;
-        public DefaultStates default_states;
+        @JsonProperty("device_id")
+        public String deviceId;
 
-        public class DefaultStates {
+        @JsonProperty("fw_ver")
+        public String fwVer;
+
+        @JsonProperty("hw_ver")
+        public String hwVer;
+
+        @JsonProperty("type")
+        public String type;
+
+        @JsonProperty("model")
+        public String model;
+
+        @JsonProperty("mac")
+        public String mac;
+
+        @JsonProperty("hw_id")
+        public String hwId;
+
+        @JsonProperty("fw_id")
+        public String fwId;
+
+        @JsonProperty("oem_id")
+        public String oemId;
+
+        @JsonProperty("ip")
+        public String ip;
+
+        @JsonProperty("time_diff")
+        public int timeDiff;
+
+        @JsonProperty("ssid")
+        public String ssid;
+
+        @JsonProperty("rssi")
+        public String rssi;
+
+        @JsonProperty("signal_level")
+        public String signalLevel;
+
+        @JsonProperty("latitude")
+        public String latitude;
+
+        @JsonProperty("longitude")
+        public String longitude;
+
+        @JsonProperty("lang")
+        public String lang;
+
+        @JsonProperty("avatar")
+        public String avatar;
+
+        @JsonProperty("region")
+        public String region;
+
+        @JsonProperty("specs")
+        public String specs;
+
+        @JsonProperty("nickname")
+        public String nickname;
+
+        @JsonProperty("has_set_location_info")
+        public boolean hasSetLocationInfo;
+
+        @JsonProperty("device_on")
+        public boolean deviceOn;
+
+        @JsonProperty("on_time")
+        public int onTime;
+
+        @JsonProperty("overheated")
+        public boolean overheated;
+
+        @JsonProperty("default_states")
+        public DefaultStates defaultStates;
+
+        public static class DefaultStates {
             public String type;
             public Object state;
         }
--- src/main/java/online/labmaster/taposmartplug/metrics/TapoMetrics.java
+++ src/main/java/online/labmaster/taposmartplug/metrics/TapoMetrics.java
@@ -1,7 +1,10 @@
 package online.labmaster.taposmartplug.metrics;
 
 import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.ImmutableTag;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import online.labmaster.taposmartplug.api.inbound.DeviceInfoResponse;
 import online.labmaster.taposmartplug.service.TapoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,10 +13,25 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Component
 public class TapoMetrics {
 
     public static final Logger logger = LoggerFactory.getLogger(TapoMetrics.class);
+    public static final String TAPO_ENERGY_USAGE_CURRENT_POWER = "tapo.energyUsage.currentPower";
+    public static final String TAPO_ENERGY_USAGE_TODAY_ENERGY = "tapo.energyUsage.todayEnergy";
+    public static final String TAPO_ENERGY_USAGE_MONTH_ENERGY = "tapo.energyUsage.monthEnergy";
+    public static final String TAPO_ENERGY_USAGE_TODAY_RUNTIME = "tapo.energyUsage.todayRuntime";
+    public static final String TAPO_ENERGY_USAGE_MONTH_RUNTIME = "tapo.energyUsage.monthRuntime";
+    public static final String TAPO_DEVICE_INFO_ON_TIME = "tapo.deviceInfo.on_time";
+    public static final String TAPO_DEVICE_INFO_RSSI = "tapo.deviceInfo.rssi";
+    public static final String TAPO_DEVICE_INFO_DEVICE_ON = "tapo.deviceInfo.device_on";
+    public static final String DEVICE_ID = "device_id";
+    public static final String IP = "ip";
+    public static final String NICKNAME = "nickname";
 
     @Autowired
     private MeterRegistry registry;
@@ -30,16 +48,25 @@ public class TapoMetrics {
         try {
             tapoMetricsData.setEnergyUsageResponse(tapoService.energyUsed());
             tapoMetricsData.setDeviceInfoResponse(tapoService.deviceInfo());
-            Gauge.builder("tapo.currentPower", tapoMetricsData, energyUsage -> tapoMetricsData.getEnergyUsage().currentPower)
-                    .tag("device_id", tapoMetricsData.getDeviceInfo().device_id)
-                    .tag("rssi", tapoMetricsData.getDeviceInfo().rssi)
-                    .tag("device_on", String.valueOf(tapoMetricsData.getDeviceInfo().device_on))
-                    .tag("overheated", String.valueOf(tapoMetricsData.getDeviceInfo().overheated))
-                    .register(registry);
+            Gauge.builder(TAPO_ENERGY_USAGE_CURRENT_POWER, tapoMetricsData, energyUsage -> tapoMetricsData.getEnergyUsage().currentPower).tags(buildPlugTags(tapoMetricsData.getDeviceInfo())).register(registry);
+            Gauge.builder(TAPO_ENERGY_USAGE_TODAY_ENERGY, tapoMetricsData, energyUsage -> tapoMetricsData.getEnergyUsage().todayEnergy).tags(buildPlugTags(tapoMetricsData.getDeviceInfo())).register(registry);
+            Gauge.builder(TAPO_ENERGY_USAGE_MONTH_ENERGY, tapoMetricsData, energyUsage -> tapoMetricsData.getEnergyUsage().monthEnergy).tags(buildPlugTags(tapoMetricsData.getDeviceInfo())).register(registry);
+            Gauge.builder(TAPO_ENERGY_USAGE_TODAY_RUNTIME, tapoMetricsData, energyUsage -> tapoMetricsData.getEnergyUsage().todayRuntime).tags(buildPlugTags(tapoMetricsData.getDeviceInfo())).register(registry);
+            Gauge.builder(TAPO_ENERGY_USAGE_MONTH_RUNTIME, tapoMetricsData, energyUsage -> tapoMetricsData.getEnergyUsage().monthRuntime).tags(buildPlugTags(tapoMetricsData.getDeviceInfo())).register(registry);
+            Gauge.builder(TAPO_DEVICE_INFO_ON_TIME, tapoMetricsData, energyUsage -> tapoMetricsData.getDeviceInfo().onTime).tags(buildPlugTags(tapoMetricsData.getDeviceInfo())).register(registry);
+            Gauge.builder(TAPO_DEVICE_INFO_RSSI, tapoMetricsData, energyUsage -> Double.parseDouble(tapoMetricsData.getDeviceInfo().rssi)).tags(buildPlugTags(tapoMetricsData.getDeviceInfo())).register(registry);
+            Gauge.builder(TAPO_DEVICE_INFO_DEVICE_ON, tapoMetricsData, energyUsage -> tapoMetricsData.getDeviceInfo().deviceOn ? 1 : 0).tags(buildPlugTags(tapoMetricsData.getDeviceInfo())).register(registry);
         } catch (Exception e) {
             logger.error("cannot retrieve tapo metrics", e);
         }
+    }
 
-
+    private Iterable<Tag> buildPlugTags(DeviceInfoResponse.DeviceInfo deviceInfo) {
+        Objects.requireNonNull(deviceInfo);
+        List<Tag> tags = new ArrayList<>();
+        tags.add(new ImmutableTag(DEVICE_ID, deviceInfo.deviceId));
+        tags.add(new ImmutableTag(IP, deviceInfo.ip));
+        tags.add(new ImmutableTag(NICKNAME, deviceInfo.nickname));
+        return tags;
     }
 }
