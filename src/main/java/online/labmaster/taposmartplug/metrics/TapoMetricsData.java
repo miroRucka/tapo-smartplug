6acc5967e17512a625bb9b6c773b5995ace3fd60
Gergely Molnar [MBA] <gmolnar83@icloud.com>
2024-05-24 08:46:04 +0200

    Minor metrics improvement:
    - Make nickname of device readable with base64 decode
    - Add new current power gauge in Watt unit

--- src/main/java/online/labmaster/taposmartplug/metrics/TapoMetricsData.java
+++ src/main/java/online/labmaster/taposmartplug/metrics/TapoMetricsData.java
@@ -1,10 +1,11 @@
 package online.labmaster.taposmartplug.metrics;
 
+import online.labmaster.taposmartplug.api.inbound.CurrentPowerResponse;
 import online.labmaster.taposmartplug.exception.TapoException;
 import online.labmaster.taposmartplug.api.inbound.DeviceInfoResponse;
 import online.labmaster.taposmartplug.api.inbound.EnergyUsageResponse;
 
-public record TapoMetricsData(EnergyUsageResponse energyUsageResponse, DeviceInfoResponse deviceInfoResponse) {
+public record TapoMetricsData(EnergyUsageResponse energyUsageResponse, DeviceInfoResponse deviceInfoResponse, CurrentPowerResponse currentPowerResponse) {
 
     public EnergyUsageResponse.EnergyUsage getEnergyUsage() {
         if (energyUsageResponse != null) {
@@ -19,6 +20,13 @@ public record TapoMetricsData(EnergyUsageResponse energyUsageResponse, DeviceInf
             return deviceInfoResponse.result;
         }
         throw new TapoException("cannot get device info, response is null");
+    }
+
+    public CurrentPowerResponse.CurrentPowerInfo getCurrentPowerInfo() {
+        if (currentPowerResponse != null) {
+            return currentPowerResponse.result;
+        }
+        throw new TapoException("cannot get current power info, response is null");
     }
 
 }
